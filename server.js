66e1d448864e605e73230ae5217657065ff2aa6d
Carter Ervin <cjrox01@yahoo.com>
2022-02-09 19:28:24 -0600

    Got DB connection working had to read sequelize docs to find alternative to lesson

--- server.js
+++ server.js
@@ -1,20 +1,34 @@
 // DEPENDENCIES
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const {Sequelize} = require('sequelize');
 
 // CONFIGURATION / MIDDLEWARE
-require('dotenv').config()
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
+require('dotenv').config();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+const sequelize = new Sequelize('tour','postgres','codeking420', {
+    host:'localhost',
+    port:5432,
+    dialect:'postgres',
+});
+
+(async () => {
+    try {
+        await sequelize.authenticate();
+        console.log('Connected to DB');
+    } catch (err){console.log('Unable to connect!', err)}
+})();
+
 
 // ROOT
 app.get('/', (req, res) => {
     res.status(200).json({
         message: 'Welcome to the Tour API'
-    })
-})
+    });
+});
 
 // LISTEN
 app.listen(process.env.PORT, () => {
-    console.log(`🎸 Rockin' on port: ${process.env.PORT}`)
-})
+    console.log(`🎸 Rockin' on port: ${process.env.PORT}`);
+});
